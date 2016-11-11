308e2bef936fc3084bc2306f1309ea83e3ca9a67
hdemon <zeitdiebe@gmail.com>
2016-11-11 20:17:15 +0900

    refactor dfa

--- src/dfa.scm
+++ src/dfa.scm
@@ -1,3 +1,6 @@
-(define (get-next-state state-transition-table current-state input)
-  (nth (car (nth (state-transition-table) (+ 1 current-state))) ; リストの0番目は入力リストだから
+(define (get-next-state state-transition-table current-state-index input)
+  (nth (get-state-row state-transition-table current-state-index)
     (get-list-index-contains-list (car (state-transition-table)) input)))
+
+(define (get-state-row state-transition-table current-state-index)
+  (car (nth (state-transition-table) (+ 1 current-state-index)))) ; リストの0番目は入力リストだから
