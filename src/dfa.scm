ee8b0ed0657de9678bc2a941ebb9d24567dd7b07
hdemon <zeitdiebe@gmail.com>
2016-11-11 20:29:53 +0900

    refactor

--- src/dfa.scm
+++ src/dfa.scm
@@ -4,3 +4,16 @@
 
 (define (get-state-row state-transition-table current-state-index)
   (car (nth (state-transition-table) (+ 1 current-state-index)))) ; リストの0番目は入力リストだから
+
+(define (match current-state input state-transition-table)
+  (let ((matched (list)))
+    (define (recurse current-state input index accepted-index)
+      (let ((next-state (get-next-state state-transition-table current-state (car input))))
+        (if (null? next-state)
+          (take (reverse matched) accepted-index)
+          (begin
+            (push! matched (car input))
+            (if (null? (cdr input))
+              (take (reverse matched) (+ index 1))
+              (recurse next-state (cdr input) (+ index 1) (if (accept? state-transition-table next-state) (+ index 1) accepted-index) ))))))
+    (recurse current-state input 0 0)))
