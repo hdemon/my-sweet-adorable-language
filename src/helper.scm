3a7ad614dbeb64b46f5246fe4740826fa28e0b6f
hdemon <zeitdiebe@gmail.com>
2017-03-22 15:30:29 +0900

    unite recognizer to helper

--- src/helper.scm
+++ src/helper.scm
@@ -1,8 +1,8 @@
-(define (real-number-recognizer input)
+(define (real-number? input)
   (dfa state-transition-table-of-real-number input))
 
-(define (bracket-recognizer input)
+(define (bracket? input)
   (dfa state-transition-table-of-bracket input))
 
-(define (identifier-recognizer input)
+(define (identifier? input)
   (dfa state-transition-table-of-identifier input))
